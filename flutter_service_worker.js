8230a031ff97fb82522370e91928ab337a969b0f
MaciejWardziak <maciejwardziak8@gmail.com>
2026-07-04 10:26:37 +0200

    Kolejna próba poprawy cachowania

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -49,7 +49,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+        Object.keys(RESOURCES).map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
